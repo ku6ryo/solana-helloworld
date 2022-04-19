266654ffa63206973ebaabafd0a2997076150775
Ryo Kuroyanagi <ku6ryo@gmail.com>
2022-04-19 14:24:14 +0900

    try to polyfill buffer

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import "sanitize.css"
 
+import { Buffer } from "buffer"
+
+globalThis.Buffer = Buffer
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
